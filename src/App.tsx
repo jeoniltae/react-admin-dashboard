d5d0ef982d2db985cd5577f323d40b93171039e1
jeoniltae <foodchain@naver.com>
2025-12-16 22:26:33 +0900

    feat: 페이지 라우터 및 공통 레이아웃 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,57 @@
-function App() {
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import "./styles/global.scss";
+import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Login from "./pages/login/Login";
 
-  return (
-    <div>Hello World</div>
-  )
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menu-container">
+            <Menu />
+          </div>
+          <div className="content-container">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "users",
+          element: <Users />,
+        },
+        {
+          path: "products",
+          element: <Products />,
+        },
+      ],
+    },
+    {
+      path: "login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
