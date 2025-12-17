fe8321520e432d887a5848cd51701e11d279ef58
jeoniltae <foodchain@naver.com>
2025-12-17 23:06:55 +0900

    feat: 레이아웃 style작업 및 Home컴포넌트의 Top Deals 영역 진행중

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+import { menu } from "../../data";
 import "./menu.scss";
 
 const Menu = () => {
-  return <div className="menu">menu</div>;
+  return (
+    <div className="menu">
+      {menu.map((item) => {
+        return (
+          <div className="item" key={item.id}>
+            <span className="title">{item.title}</span>
+            {item.listItems.map((listItem) => {
+              return (
+                <Link key={listItem.id} to={listItem.url} className="listItem">
+                  <img src={listItem.icon} alt={listItem.title} />
+                  <span className="listItemTitle">{listItem.title}</span>
+                </Link>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Menu;
