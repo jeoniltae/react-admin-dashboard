f3591189c5d72f335c9e2609fafc1008c75d0d53
jeoniltae <foodchain@naver.com>
2025-12-29 00:11:59 +0900

    update: Users 화면 데이터 테이블 그리드 작업

---
+++ src/components/dataTable/DataTable.tsx
@@ -0,0 +1,69 @@
+import { Link } from "react-router-dom";
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
+import "./dataTable.scss";
+
+type Props = {
+  columns: GridColDef[];
+  rows: object[];
+  slug: string;
+};
+
+const DataTable = (props: Props) => {
+  const handleDelete = (id: number) => {
+    console.log(id + " has been deleted");
+  };
+
+  const actionColumn: GridColDef = {
+    field: "actions",
+    headerName: "Actions",
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <div className="action">
+          <Link to={`/${props.slug}/${params.row.id}`}>
+            <img src="/view.svg" alt="" />
+          </Link>
+          <div className="delete" onClick={() => handleDelete(params.row.id)}>
+            <img src="/delete.svg" alt="" />
+          </div>
+        </div>
+      );
+    },
+  };
+  return (
+    <div className="dataTable">
+      <DataGrid
+        className="dataGrid"
+        rows={props.rows}
+        columns={[...props.columns, actionColumn]}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        slots={{
+          toolbar: GridToolbar,
+        }}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
+            quickFilterProps: {
+              debounceMs: 500,
+            },
+          },
+        }}
+        showToolbar
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+        disableColumnFilter
+        disableColumnSelector
+        disableDensitySelector
+      />
+    </div>
+  );
+};
+
+export default DataTable;
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,83 @@
+import { GridColDef } from "@mui/x-data-grid";
+import DataTable from "../../components/dataTable/DataTable";
+import { userRows } from "../../data";
 import "./users.scss";
 
+const columns: GridColDef<(typeof userRows)[number]>[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="avatar" />;
+    },
+  },
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 180,
+    editable: true,
+  },
+  {
+    field: "phone",
+    headerName: "Phone",
+    width: 120,
+    editable: true,
+  },
+  {
+    field: "createdAt",
+    headerName: "CreatedAt",
+    width: 160,
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 100,
+    type: "boolean",
+  },
+];
+
+// const rows = [
+//   { id: 1, lastName: "Snow", firstName: "Jon", age: 14, status: true },
+//   {
+//     id: 2,
+//     lastName: "Lannister",
+//     firstName: "Cersei",
+//     age: 31,
+//     status: false,
+//   },
+//   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
+//   { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
+//   { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+//   { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+//   { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+//   { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+//   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+// ];
+
 const Users = () => {
-  return <div className="users">users</div>;
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+      <DataTable slug="users" columns={columns} rows={userRows} />
+    </div>
+  );
 };
 
 export default Users;
